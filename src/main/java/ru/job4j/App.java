b6f89c4b30dad21cb028ec3a5a8cd1b45fbbd942
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2022-03-25 21:52:34 +0300

    added .travis.yml, checkstyle.xml, README.md

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -1,13 +1,11 @@
 package ru.job4j;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public int someLogic() {
+        return 1;
     }
 }
